378fd52fce413d24b7934bc3f9bddaa8a9645846
Guilherme Francisco <guilhermesf2008@hotmail.com>
2017-11-12 18:46:54 -0200

    Add files via upload

---
+++ Java/pojo/Autenticacao.java
@@ -0,0 +1,33 @@
+package pojo;
+
+public class Autenticacao {
+
+	private String login;
+	private String senha;
+	private int tipo;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+}
