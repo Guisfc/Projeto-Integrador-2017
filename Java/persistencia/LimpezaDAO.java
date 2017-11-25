f856320974cc0573b2fe02e8c2e5573e301eb303
Guilherme Francisco <guilhermesf2008@hotmail.com>
2017-11-24 23:51:47 -0200

    Add files via upload

--- Java/persistencia/LimpezaDAO.java
+++ Java/persistencia/LimpezaDAO.java
@@ -3,29 +3,37 @@ package persistencia;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import pojo.Funcionario;
+
 
 import pojo.Limpeza;
+import pojo.Quarto;
 
 public class LimpezaDAO {
 
 	Conexao conexao;
 
 	public LimpezaDAO() {
-		this.conexao = new Conexao("localhost", "bd_hotel", "root", "root");
+		this.conexao = new Conexao("localhost", "bd_hotel", "root", "");
 	}
 
 	public Limpeza salvar(Limpeza limpeza) {
 		String sql = "INSERT INTO limpeza (cod_limpeza, data_hora, descricao, id_funcionario, id_quarto) VALUES (null,?,?,?,?)";
 		conexao.abrirConexao();
 		try {
-			PreparedStatement stmt = conexao.getConexao().prepareStatement(sql);
+			PreparedStatement stmt = conexao.getConexao().prepareStatement(sql,  Statement.RETURN_GENERATED_KEYS);
 			stmt.setTimestamp(1, new java.sql.Timestamp(limpeza.getDataSql().getTime()));	//(1, new java.sql.Date(limpeza.getDataSql().getTime());  << SOMENTE DATA
 			stmt.setString(2, limpeza.getDescricao());
-			stmt.setLong(3, limpeza.getIdFuncionario());
-			stmt.setLong(4, limpeza.getIdQuarto());
+			stmt.setLong(3, limpeza.getFuncionario().getIdFuncionario());
+			stmt.setLong(4, limpeza.getQuarto().getIdQuarto());
 			stmt.executeUpdate();
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				limpeza.setCodLimpeza(rs.getLong(1));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -55,8 +63,8 @@ public class LimpezaDAO {
 			PreparedStatement stmt = conexao.getConexao().prepareStatement(sql);
 			stmt.setTimestamp(1, new java.sql.Timestamp(limpeza.getDataSql().getTime()));
 			stmt.setString(2, limpeza.getDescricao());
-			stmt.setLong(3, limpeza.getIdFuncionario());
-			stmt.setLong(4, limpeza.getIdQuarto());
+			stmt.setLong(3, limpeza.getFuncionario().getIdFuncionario());
+			stmt.setLong(4, limpeza.getQuarto().getIdQuarto());
 			stmt.setLong(5, limpeza.getCodLimpeza());
 			stmt.executeUpdate();
 		} catch (SQLException e) {
@@ -69,7 +77,8 @@ public class LimpezaDAO {
 
 	public List<Limpeza> pesquisar() {
 		List<Limpeza> listaLimpeza = new ArrayList<Limpeza>();
-		String sql = "SELECT * FROM limpeza";
+                String sql = "SELECT cod_limpeza, data_hora, descricao, nome, sobrenome, id_quarto FROM limpeza INNER JOIN funcionario ON limpeza.id_funcionario=funcionario.id_funcionario";
+		//String sql = "SELECT * FROM limpeza";
 		conexao.abrirConexao();
 		try {
 			PreparedStatement stmt = conexao.getConexao().prepareStatement(sql);
@@ -80,9 +89,16 @@ public class LimpezaDAO {
 				limpeza.setCodLimpeza(rs.getLong("cod_limpeza"));
 				limpeza.setDataSql(rs.getTimestamp("data_hora"));
 				limpeza.setDescricao(rs.getString("descricao"));
-				limpeza.setIdFuncionario(rs.getLong("id_funcionario"));
-				limpeza.setIdQuarto(rs.getLong("id_quarto"));
+                                
+                                Funcionario funcionario = new Funcionario();
+                                funcionario.setNome(rs.getString("nome"));
+                                funcionario.setSobrenome(rs.getString("sobrenome"));
+                                
+                                Quarto quarto = new Quarto();
+				quarto.setIdQuarto(rs.getLong("id_quarto"));
 
+                                limpeza.setFuncionario(funcionario);
+                                limpeza.setQuarto(quarto);
 				listaLimpeza.add(limpeza);
 			}
 
